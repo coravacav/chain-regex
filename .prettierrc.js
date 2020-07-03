20ad7c0c7f869cd294db9e78faa432795693f753
Stefan Todorov <stefan.b.todorov@gmail.com>
2020-07-03 06:22:01 -0600

    basic repo

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'es5',
+    printWidth: 120,
+    endOfLine: 'auto',
+    arrowParens: 'always',
+};
